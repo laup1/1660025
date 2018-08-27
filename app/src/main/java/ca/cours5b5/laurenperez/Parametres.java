e43ad35b4674dfac8d0bf2704b479d5bd5cd99ee
lauren perez <laurent_perez@hotmail.com>
2018-08-27 12:59:16 -0400

    Atelier 2 Langue et orientation

--- app/src/main/java/ca/cours5b5/laurenperez/Parametres.java
+++ app/src/main/java/ca/cours5b5/laurenperez/Parametres.java
@@ -3,6 +3,7 @@ package ca.cours5b5.laurenperez;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class Parametres extends AppCompatActivity {
 
@@ -10,6 +11,12 @@ public class Parametres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
-        Log.d("MonEtiquette","Bonjour!");
+
+
+        Log.d("log",this.getResources().getString(R.string.hello_world));
+        Log.d("log",this.getResources().getString(R.string.orientation));
+
+
+
     }
 }
