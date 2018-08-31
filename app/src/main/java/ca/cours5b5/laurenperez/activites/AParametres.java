d65961d06e61741b4f0fcbec06a820ee5964472f
lauren perez <laurent_perez@hotmail.com>
2018-08-31 11:32:00 -0400

    Revert "Atelier03 création d'une Vue""
    This reverts commit 04aa368c6e1a7a653af054099eaf9e958da7517c.

---
+++ app/src/main/java/ca/cours5b5/laurenperez/activites/AParametres.java
@@ -0,0 +1,54 @@
+package ca.cours5b5.laurenperez.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+
+import ca.cours5b5.laurenperez.R;
+
+public class AParametres extends AppCompatActivity {
+
+    private Spinner spinnerHauteur, spinnerLargeur, spinnerPourGagner ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametres);
+
+        spinnerHauteur = (Spinner) findViewById(R.id.spinnerHauteur);
+
+        ArrayAdapter<Integer> adapterHauteur = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item);
+        spinnerHauteur.setAdapter(adapterHauteur);
+        adapterHauteur.addAll(4,5,6,7,8,9,10);
+        spinnerHauteur.setSelection(2);
+
+
+        spinnerLargeur = (Spinner) findViewById(R.id.spinnerLargeur);
+
+        ArrayAdapter<Integer> adapterLargeur = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item);
+        spinnerLargeur.setAdapter(adapterLargeur);
+        adapterLargeur.addAll(4,5,6,7,8,9,10);
+        spinnerLargeur.setSelection(3);
+
+
+        spinnerPourGagner = (Spinner) findViewById(R.id.spinnerPourGagner);
+
+        ArrayAdapter<Integer> adapterPourGagner = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item);
+        spinnerPourGagner.setAdapter(adapterPourGagner);
+        adapterPourGagner.addAll(3,4);
+        spinnerPourGagner.setSelection(1);
+
+
+        Log.d("log",this.getResources().getString(R.string.hello_world));
+        Log.d("log",this.getResources().getString(R.string.orientation));
+
+
+
+    }
+}
